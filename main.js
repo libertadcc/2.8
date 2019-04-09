b40436f93bb19f1a5cf2a704eb214d113bbd4801
Libertad Chapinal <libertadchapinalcervantes@gmail.com>
2019-04-09 11:45:52 +0200

    rainbow

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ const head = document.querySelector('.head');
 
 function wink() {
     face.innerHTML=`;P`;
+    head.classList.add('red');
+    head.classList.remove('rain');
 }
 function normal() {
     face.innerHTML=`:)`;
+    head.classList.remove('red','rain');
 }
 function tongue() {
     face.innerHTML=`;)`;
+    head.classList.remove('red');
+    head.classList.add('rain');
 }
 
 head.addEventListener('click', wink);
